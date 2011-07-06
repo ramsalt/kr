f916018b535e9fa2b93ed19cc35fee6805120521
yngve <yngve@4fc3c221-e4bb-45c9-97bc-c54e76f77a07>
2011-07-06 07:54:17 +0000

    Refresh after 600 sec

--- themes/kr2011/page-header.tpl.php
+++ themes/kr2011/page-header.tpl.php
@@ -26,6 +26,7 @@
   <?php print $op_head; ?>
   <?php print $styles ?>
   <?php print $scripts ?>
+  <meta http-equiv="refresh" content="600" />
   <!--[if gte IE 6]><?php print openpublish_get_ie_styles(); ?><![endif]-->  
   <!--[if IE 6]><?php print openpublish_get_ie6_styles(); ?><![endif]-->
 </head>
