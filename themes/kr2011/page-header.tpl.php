c521adf7dc8fed389ddad6d63723c0f42e6730fd
ole <ole@4fc3c221-e4bb-45c9-97bc-c54e76f77a07>
2011-07-28 14:26:21 +0000

    page endringer

--- themes/kr2011/page-header.tpl.php
+++ themes/kr2011/page-header.tpl.php
@@ -9,6 +9,17 @@
  * @ingroup page
  */
 ?>
+<!--
+Hei, du, HTML-kildekodeleser! :-)
+
+Denne nettsiden er laget ved hjelp av fri programvare. Rammeverket er Drupal.
+PHP er programmeringsspråket og databasen er MySQL. Herligheten bruker Apache som webserver og Varnish som HTTP akselerator.
+
+Har du noen spørsmål svarer vi gjerne på dem, bare kontakt oss på www.ramsalt.com
+
+Ha en fin dag!
+
+-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html 
      xmlns="http://www.w3.org/1999/xhtml"      
@@ -22,17 +33,7 @@
      dir="<?php print $language->dir; ?>"
      version="XHTML+RDFa 1.0" >
 <head>
-  <!--
-Hei, du, HTML-kildekodeleser! :-)
 
-Denne nettsiden er laget ved hjelp av fri programvare. Rammeverket er Drupal.
-PHP er programmeringsspråket og databasen er MySQL. Herligheten bruker Apache som webserver og Varnish som HTTP akselerator.
-
-Har du noen spørsmål svarer vi gjerne på dem, bare kontakt oss på www.ramsalt.com
-
-Ha en fin dag!
-
--->
   <title><?php print $head_title ?></title>
   <?php print $op_head; ?>
   <?php print $styles ?>
