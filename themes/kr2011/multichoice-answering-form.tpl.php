967de0df4e717f6efad76c57346da0d658362114
ole <ole@4fc3c221-e4bb-45c9-97bc-c54e76f77a07>
2011-08-05 09:26:05 +0000

    theme greier

--- themes/kr2011/multichoice-answering-form.tpl.php
+++ themes/kr2011/multichoice-answering-form.tpl.php
@@ -9,8 +9,12 @@
  */
 //print '<pre>';print_r($form);print '</pre>';
 
-print '<h2>'.$form['#parameters'][2]->title.'</h2>';
-print '<p>'.$form['#parameters'][2]->teaser.'</h2>';
+if(strlen($form['#parameters'][2]->title>2)){
+  print '<h2>'.$form['#parameters'][2]->title.'</h2>';
+}
+if(strlen($form['#parameters'][2]->teaser>2)){
+  print '<p>'.$form['#parameters'][2]->teaser.'</h2>';
+}
 print drupal_render($form);
 
 ?>
