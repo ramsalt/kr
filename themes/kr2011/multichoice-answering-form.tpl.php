a5f6969ae65fd36457fd60192a06424f39b9507b
ole <ole@4fc3c221-e4bb-45c9-97bc-c54e76f77a07>
2011-08-05 09:21:51 +0000

    quiz

--- themes/kr2011/multichoice-answering-form.tpl.php
+++ themes/kr2011/multichoice-answering-form.tpl.php
@@ -10,6 +10,7 @@
 //print '<pre>';print_r($form);print '</pre>';
 
 print '<h2>'.$form['#parameters'][2]->title.'</h2>';
+print '<p>'.$form['#parameters'][2]->teaser.'</h2>';
 print drupal_render($form);
 
 ?>
