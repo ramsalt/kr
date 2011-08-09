f6812366e6a9bd438105d5e6722e2957c0f101dc
ole <ole@4fc3c221-e4bb-45c9-97bc-c54e76f77a07>
2011-08-09 07:25:55 +0000

    wordcount

--- modules/kr_wordcount/jquery.jqEasyCharCounter.js
+++ modules/kr_wordcount/jquery.jqEasyCharCounter.js
@@ -48,7 +48,7 @@ $.fn.extend({
 			
 			// bind events to this element
 			$this
-				.bind('keydown keyup keypress', doCount)
+				.bind('keypress', doCount)
 				.bind('focus paste', function(){setTimeout(doCount, 10);})
 				.bind('blur', function(){jqEasyCounterMsg.stop().fadeTo( 'fast', 0);return false;});
 			
