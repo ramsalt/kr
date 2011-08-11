10dfbe72e8b52b741de02c1a8103327da2c8dbbf
ole <ole@4fc3c221-e4bb-45c9-97bc-c54e76f77a07>
2011-08-11 11:03:05 +0000

    theme

--- themes/kr2011/js/kr2011.js
+++ themes/kr2011/js/kr2011.js
@@ -5,9 +5,24 @@ Drupal.behaviors.run_after_page_loads = function (context) {
 
 			var center_height = $('#center').height();
 			var sidebar_left_height = $('#sidebar-left').height();
+			var sidebar_right_height = $('#sidebar-right').height();
 			
-			if(sidebar_left_height<center_height){
-				$('#sidebar-left').height(center_height);
+			var sidebars = false;
+			var max = center_height;
+			
+			if(sidebar_right_height > center_height || sidebar_left_height > center_height){
+			  sidebars = true;
+			  if(sidebar_right_height > sidebar_left_height){
+			    max = sidebar_right_height;
+			  }else{
+			    max = sidebar_left_height;
+			  }
 			}
+			
+			
+			
+			$('#sidebar-left').height(max);
+			$('#sidebar-right').height(max);
+		
 }
 })(jQuery);
