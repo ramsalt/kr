e812c849effd183d53872e8872ab844aaf627c45
martin <martin@4fc3c221-e4bb-45c9-97bc-c54e76f77a07>
2011-12-07 10:27:47 +0000

    Fixed some issues on statitistics feature

--- modules/features/statistics_node/js/statisticsTable.js
+++ modules/features/statistics_node/js/statisticsTable.js
@@ -1,10 +1,9 @@
 Drupal.behaviors.statisticsTable = function (context) {
 	$('.data-table').dataTable({
-		"iDisplayLength": 25,
+		"iDisplayLength": 50,
 					"sPaginationType": "full_numbers",
 		"oLanguage": {
 			"sProcessing":   "Laster...",
-			"sLengthMenu":   "Vis _MENU_ linjer",
 			"sZeroRecords":  "Ingen linjer matcher søket",
 			"sInfo":         "Viser _START_ til _END_ av _TOTAL_ linjer",
 			"sInfoEmpty":    "Viser 0 til 0 av 0 linjer",
@@ -12,6 +11,14 @@ Drupal.behaviors.statisticsTable = function (context) {
 			"sInfoPostFix":  "",
 			"sSearch":       "Søk:",
 			"sUrl":          "",
+			"sLengthMenu": 'Vis <select>'+
+				'<option value="25">25</option>'+
+				'<option value="50">50</option>'+
+				'<option value="75">75</option>'+
+				'<option value="100">100</option>'+
+				'<option value="-1">Alle</option>'+
+				'</select> linjer',
+		
 		  "oPaginate": {
 			    "sFirst":    "Første",
 			    "sPrevious": "Forrige",
