6f678ff7c8060c75b19368aa4e698cb2ba3feb5e
olemsa <olemahtte@gmail.com>
2012-04-03 10:38:27 +0200

    mod div

--- sites/default/themes/kr2011/template.php
+++ sites/default/themes/kr2011/template.php
@@ -349,10 +349,10 @@ function kr2011_preprocess_node(&$vars){
       $html .='</a>';
       $html .='</li>';
       $image = image_get_info(imagecache_create_path('slider_stort', $vars['field_galleri_bilde'][$delta]['filepath']));
-
+//<img src="'.imagecache_create_path('slider_stort', $vars['field_galleri_bilde'][$delta]['filepath']).'">
       $hidden.= '<div id="bilde_'.$vars['nid'].'_'.$delta.'">
       '.theme_imagecache('slider_stort', $vars['field_galleri_bilde'][$delta]['filepath']).'
-      <img src="'.imagecache_create_path('slider_stort', $vars['field_galleri_bilde'][$delta]['filepath']).'">
+      
       <div class="caption" style="width: '.$image['width'].'px;">'.$vars['field_galleri_desc'][$delta]['safe'].'</div>
       </div>';
     }
