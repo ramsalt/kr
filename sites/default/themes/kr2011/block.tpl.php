00062324cc9a29e4922b82f5fd9bcb67da743d90
Ole Mahtte Scheie Anti <ole@ramsalt.com>
2012-09-07 14:45:08 +0200

    block fixer

--- sites/default/themes/kr2011/block.tpl.php
+++ sites/default/themes/kr2011/block.tpl.php
@@ -1,13 +1,14 @@
 <?php
-// $Id: block.tpl.php,v 1.3 2007/08/07 08:39:36 goba Exp $
-
+// $Id: block.tpl.php,v 1.1.2.2 2010/12/01 18:06:49 tirdadc Exp $
 ?>
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clearfix block block-<?php print $block->module ?>">
 
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?>">
+  <?php if (!empty($block->subject) && $block->module != 'views'): ?>
+    <h2><?php print "$block->subject"; ?></h2>
+  <?php endif;?>
 
-<?php if (!empty($block->subject) && false): ?>
-  <h2><?php print $block->subject ?></h2>
-<?php endif;?>
+  <div class="content">
+    <?php print $block->content ?>
+  </div>
 
-  <div class="content"><?php print $block->content ?></div>
 </div>
