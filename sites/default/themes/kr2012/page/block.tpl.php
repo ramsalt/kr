a08ee2b54cdf33dfc6c48c39a5544e27709e3957
Ole Mahtte Scheie Anti <ole@ramsalt.com>
2012-11-19 11:18:35 +0100

    s

--- sites/default/themes/kr2012/page/block.tpl.php
+++ sites/default/themes/kr2012/page/block.tpl.php
@@ -1,7 +1,7 @@
-<?php
+	<?php
 // $Id: block.tpl.php,v 1.1.2.2 2010/12/01 18:06:49 tirdadc Exp $
 ?>
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clearfix block block-<?php print $block->module ?> <?php print $block_classes; ?>">
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clearfix block block-<?php print $block->module ?> <?php print $block_classes; ?> <?php print $context_block_classes; ?>">
   <?php if (!empty($block->subject) && $block->module != 'views'): ?>
     <h2><?php print "$block->subject"; ?></h2>
   <?php endif;?>
