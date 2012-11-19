955e071afacd8f8e8c61182ff0775390ef4b0fe6
Ole Mahtte Scheie Anti <ole@ramsalt.com>
2012-11-19 15:22:01 +0100

    div

--- sites/default/themes/kr2012/js/front.js
+++ sites/default/themes/kr2012/js/front.js
@@ -15,12 +15,15 @@
 			$(selector).height('auto');
 		}
 	}
-	function eq_one(){
+	function eq_desk(){
 		if($('body').hasClass('layout-desktop')){
 			$('.container-16 .grid.full.nomargin .one').height('auto');
 			$('.container-16 .grid.full.nomargin .one').height($('.container-16 .grid.full.nomargin').height());
-
+			$('.newslist.sak-7 li.views-row').height('auto');
+			console.log($('.newslist.sak-7 .item-list ul').height());
+			$('.newslist.sak-7 .item-list li.views-row').height($('.newslist.sak-7 .item-list ul').height());
 		}else{
+			$('.newslist.sak-7 .item-list li.views-row').height('auto');
 			$('.container-16 .grid.full.nomargin .one').height('auto');
 		}
 	}
@@ -30,12 +33,12 @@
 			
 		});
 		$(window).resize(function(){
-			eq_one();
+			eq_desk();
 		});
 		equal_height('.newslist.sak-4 li.views-row');
 
 	});
 	$(window).load(function() {
-		eq_one();
+		eq_desk();
 	});
 }(jQuery));
