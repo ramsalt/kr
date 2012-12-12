355e07ecf6fb9e7b350885d3f883e75d8bfa958f
Ole Mahtte Scheie Anti <ole@ramsalt.com>
2012-12-12 16:18:26 +0100

    mob header

--- sites/default/themes/kr2012/js/meny.js
+++ sites/default/themes/kr2012/js/meny.js
@@ -3,13 +3,42 @@
 		$('body').bind('layoutset', function(event, layout) {
 			$('#nav-button').removeClass('open');
 			$('#nav').removeClass('open');
+			$('#search-content').removeClass('open');
+			$("#search-mob").removeClass('open');
+			$("#user-login-mob").removeClass('open');
+			$("#login-content").removeClass('open');
 		});
-		$('body.layout-mobile #nav-button a, body.layout-tablet #nav-button a').bind('click', function(){
+		$('body.layout-mobile #nav-button a').bind('click', function(){
+			$("#user-login-mob").removeClass('open');
+			$("#login-content").removeClass('open');
+			$('#search-content').removeClass('open');
+			$("#search-mob").removeClass('open');
+			
 			$(this).parent().toggleClass('open');
 			$("#nav").toggleClass('open');
 			return false;
 		});
-		$('body.layout-mobile #nav .multilevel a.first-level, body.layout-tablet #nav .multilevel a.first-level').bind('click', function(){
+		$('body.layout-mobile #search-content a').bind('click', function(){
+			$('#nav-button').removeClass('open');
+			$('#nav').removeClass('open');
+			$("#user-login-mob").removeClass('open');
+			$("#login-content").removeClass('open');
+			
+			$(this).parent().toggleClass('open');
+			$("#search-mob").toggleClass('open');
+			return false;
+		});
+		$('body.layout-mobile #login-content a').bind('click', function(){
+			$('#nav-button').removeClass('open');
+			$('#nav').removeClass('open');
+			$('#search-content').removeClass('open');
+			$("#search-mob").removeClass('open');
+			
+			$(this).parent().toggleClass('open');
+			$("#user-login-mob").toggleClass('open');
+			return false;
+		});
+		$('body.layout-mobile #nav .multilevel a.first-level').bind('click', function(){
 			$(this).parent().toggleClass('open');
 			return false;
 		});
