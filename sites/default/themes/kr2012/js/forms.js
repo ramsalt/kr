f22150945262ecbd181a6ecba047ad3592407598
Ole Mahtte Scheie Anti <ole@ramsalt.com>
2012-12-14 15:37:37 +0100

    theme fix

---
+++ sites/default/themes/kr2012/js/forms.js
@@ -0,0 +1,14 @@
+(function ($) {
+	$(document).ready(function() {
+		$('.form-label-alter .form-item input').bind('focus.forms', function(){
+			form_item = $(this).parent();
+			$('label',form_item).hide();
+		});
+		$('.form-label-alter .form-item input').bind('focusout.forms', function(){
+			form_item = $(this).parent();
+			$('label',form_item).show();
+		});
+
+	});
+
+}(jQuery));
