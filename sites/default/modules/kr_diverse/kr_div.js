776ebf5039b88216812858e85f4cec878ed1df62
Ole Mahtte Scheie Anti <ole@ramsalt.com>
2012-12-14 11:51:19 +0100

    ticker

--- sites/default/modules/kr_diverse/kr_div.js
+++ sites/default/modules/kr_diverse/kr_div.js
@@ -1,6 +1,23 @@
 (function ($) {
-
+	function ticker_news_width(){
+		content_width = $('#block-kr_diverse-2 .content').width();
+		ticker_label_width = parseInt($('#block-kr_diverse-2 .ticker-label').width()) + parseInt($('#block-kr_diverse-2 .ticker-label').css('margin-left').replace('px', '')) + parseInt($('#block-kr_diverse-2 .ticker-label').css('margin-right').replace('px', ''));
+		ticker_next_width = parseInt($('#block-kr_diverse-2 .ticker-next').width()) + parseInt($('#block-kr_diverse-2 .ticker-next').css('right').replace('px', ''));
+		total_width = content_width - ticker_label_width - ticker_next_width;
+		$('#block-kr_diverse-2 .ticker-news').width(total_width-10);
+	}
 	$(document).ready(function() {
-
+		$('body').bind('layoutset', function(event, layout) {
+			ticker_news_width();
+		});
+		ticker_news_width();
+		
+		$('.ticker div.ticker-prev').bind('click.ticker', function(){
+			$('#block-kr_diverse-2 .ticker-news ul').animate({'margin-left': "+=100px"}, 50);
+			
+		});
+		$('.ticker div.ticker-next').bind('click.ticker', function(){
+			$('#block-kr_diverse-2 .ticker-news ul').animate({'margin-left': "-=100px"}, 50);
+		});
 	});
 }(jQuery));
