39fbbdb89d444bd9ce5f822be3b880315a664091
Erik Falster <erik@ramsalt.com>
2013-01-09 13:28:16 +0100

    test med subscribeblock

--- sites/default/modules/simplenews/simplenews-block.tpl.php
+++ sites/default/modules/simplenews/simplenews-block.tpl.php
@@ -38,8 +38,7 @@
 
   <?php if ($use_form): ?>
     <?php print $form; ?>
-  <?php elseif ($subscription_link): ?>
-    <p><?php print $subscription_link; ?></p>
+ 
   <?php endif; ?>
 
   <?php if ($use_issue_link && $newsletter_link): ?>
