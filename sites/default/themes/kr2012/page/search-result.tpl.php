e0a0ac04ee2e502d0332964818a4d22871919141
Erik Falster <erik@ramsalt.com>
2013-02-13 15:09:26 +0100

    search

--- sites/default/themes/kr2012/page/search-result.tpl.php
+++ sites/default/themes/kr2012/page/search-result.tpl.php
@@ -54,6 +54,9 @@
   <a href="<?php print $url; ?>"><?php print $title; ?></a>
 </dt>
 
+<span class="search-image">
+<img src="/sites/default/files/imagecache/thumbnail/<?php print $result['node']->field_main_image[0]['filepath']; ?>"/> 
+</span>
 
 <dd>
   <?php if ($snippet) : ?>
