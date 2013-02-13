652a3c2fe1e8c694e5cc88cd72430ded79412c74
Erik Falster <erik@ramsalt.com>
2013-02-13 15:44:23 +0100

    searcg

--- sites/default/themes/kr2012/page/search-result.tpl.php
+++ sites/default/themes/kr2012/page/search-result.tpl.php
@@ -57,7 +57,10 @@
 <span class="search-image">
 <img src="/sites/default/files/imagecache/thumbnail/<?php print $result['node']->field_main_image[0]['filename']; ?>"/> 
 </span>
-
+<?php print $result['node']->field_main_image[0]['filename']; ?>
+<?php
+print_r ($node);
+?>
 <dd>
   <?php if ($snippet) : ?>
     <p class="search-snippet"><?php print $snippet; ?></p>
